0ac8a2a29c0369f0740b28023dbc311858cebcaf
GoldenWind206 <dami.dami2053@gmail.com>
2025-03-06 15:19:09 +0000

    cambio

--- src/routes/root/index.tsx
+++ src/routes/root/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Link } from "react-router";
 
 export const RootRoute = () => {
   const [count, setCount] = useState(0);
@@ -42,7 +43,11 @@ export const RootRoute = () => {
 
         <p className="text-center text-sm text-blue-400">Clicca sui loghi per saperne di più</p>
       <center>
-        <a href="dettaglio/0"> link 0</a>
+        <link
+        to={'/dettaglio/$count'}
+        >
+          link { count }
+        </link>
       </center>
       </div>
     </div>
